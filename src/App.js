20b2044ea52c6980dfc2855df107cafa88bc23da
ivanroscuenca <ivanros@protonmail.com>
2023-08-23 10:29:44 +0200

    calculadora

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
 import "./App.css";
+import FreeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
+import Boton from "./componentes/Boton";
+import Pantalla from "./componentes/Pantalla";
+import BotonClear from "./componentes/BotonClear";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">Calculadora</div>;
+  //creamos un estado
+  const [input, setInput] = useState("");
+  // Agregar input
+  const agregarInput = (val) => {
+    setInput(input + val);
+  };
+
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
+          src={FreeCodeCampLogo}
+          alt="Logo de freecodecamp"
+        />
+      </div>
+      <div className="contenedor-calculadora">
+        {/* atributo de componente pantalla es igual a input del estado */}
+        <Pantalla input={input} />
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className="fila">
+          <BotonClear>CLEAR</BotonClear>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../hojas-de-estilo/Pantalla.css";
+
+//componentes sencillos con estilo flecha
+const Pantalla = ({ input }) => <div className="input">{input}</div>;
+
+export default Pantalla;
